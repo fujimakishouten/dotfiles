dc71cb8142f1b2bc23366d7416b593612a3f264f
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2017-02-06 19:53:35 +0900

    Update emacs packages

--- .emacs.d/elpa/python-mode-20161009.2250/python-mode-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; python-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (22541 27866 701458 407000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; python-mode-autoloads.el ends here
