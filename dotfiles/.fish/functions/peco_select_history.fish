f4babf28b6f0aa277b53079b4ae6be91a009c861
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2019-12-13 11:07:39 +0900

    Fix fish peco history search function.

--- dotfiles/.fish/functions/peco_select_history.fish
+++ dotfiles/.fish/functions/peco_select_history.fish
@@ -1,7 +1,11 @@
 function peco_select_history
     set LF '\\\x0A'
-    history | awk '!a[$0]++' | peco | sed 's/\\n/'"$LF"'/g'
+    history | awk '!a[$0]++' | peco | sed 's/\\n/'"$LF"'/g' | read HISTORY
 
-    commandline -f repaint
+    if test $HISTORY
+        commandline $HISTORY
+    else
+        commandline ""
+    end
 end
 
